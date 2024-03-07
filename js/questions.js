c781dd870f12fb85968bbd95931cf97ba6124346
Bumbakkomal <162156612+Bumbakkomal@users.noreply.github.com>
2024-03-07 14:51:09 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What are the types of Web Development?",
+        answer: "All the above",
+        options: [
+            "Front-End development",
+            "Back-End depment",
+            "FulStack development",
+            "All the above"
+        ]
+    },
+    
+    {
+        numb: 2,
+        question: "HTML gives a functinality to collectr data from user with the help of ?",
+        answer: "Forms",
+        options: [
+            "Hyperlink",
+            "Forms",
+            "Tables",
+            "None of the above"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Which of the following are scripting languages?",
+        answer: "All the above",
+        options: [
+            "Html",
+            "Css,PHP",
+            "JS,Ruby",
+            "All the above"
+        ]
+    },
+    
+    {
+        numb: 4,
+        question: "What is the purpose of the script tag in HTML?",
+        answer: "To include external JavaScript code",
+        options: [
+            "To define page structure",
+            "To include external CSS styles ",
+            "To include external JavaScript code",
+            "To create a site"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Which technology is primilarily responsible for the styling of web pages",
+        answer: "CSS",
+        options: [
+            "HTML",
+            "CSS",
+            "PHP",
+            "JAVASCRIPT"
+        ]
+    },
+];
